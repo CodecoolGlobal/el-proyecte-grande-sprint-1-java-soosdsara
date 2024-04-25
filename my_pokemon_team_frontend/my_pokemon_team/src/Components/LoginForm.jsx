a83452dc3d60ce91367c82068018dd7edf633698
Papp Tommy <papptomi.23@gmail.com>
2024-04-25 11:40:45 +0200

    update: update LoginForm component with useParams

--- my_pokemon_team_frontend/my_pokemon_team/src/Components/LoginForm.jsx
+++ my_pokemon_team_frontend/my_pokemon_team/src/Components/LoginForm.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams} from 'react-router-dom';
 
 
 
 function LoginForm() {
     const navigate = useNavigate();
+    const { id } = useParams();
     const [name, setName] = useState("");
     const [password, setPassword] = useState("");
 
@@ -13,20 +14,20 @@ function LoginForm() {
             <h1>Welcome Trainer in My Pokemon Team!</h1>
             <form >
                 <div>
-                    <label htmlFor="logInName"> Trainer name:
+                    <label htmlFor="loginName"> Trainer name:
                         <input type="text"
-                            name="name"
-                            id="logInName"
+                            name="loginName"
+                            id="loginName"
                             value={name}
                             onChange={e => setName(e.target.value)}
                         />
                     </label>
                 </div>
                 <div>
-                    <label htmlFor="logInPassword"> Password:
+                    <label htmlFor="loginPassword"> Password:
                         <input type="password"
-                            name="logInPassword"
-                            id="logInPassword"
+                            name="loginPassword"
+                            id="loginPassword"
                             value={password}
                             onChange={e => setPassword(e.target.value)}
                         />
@@ -35,7 +36,7 @@ function LoginForm() {
             </form>
             <div>
                 <button onClick={() => navigate("/registration")}>Sign up</button>
-                <button>Log in</button>
+                <button onClick={() => navigate(`/userpage/${id}`)}>Log in</button>
             </div>
         </div>
     )
