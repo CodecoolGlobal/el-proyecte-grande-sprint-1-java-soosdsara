b3730351c2c3cb52e728492f183cfce0efabf31a
Sara Soos <142304198+soosdsara@users.noreply.github.com>
2024-05-06 14:39:12 +0200

    refactor: optimized folder structure

---
+++ my_pokemon_team_frontend/src/Pages/UserPage.jsx
@@ -0,0 +1,27 @@
+import { useState, useEffect } from 'react'
+import { useLocation } from "react-router-dom";
+import Pokemon from '../Components/Pokemon';
+
+const UserPage = () => {
+  const trainerId = useLocation().pathname.split("/userpage/")[1];
+  const [pokemons, setPokemons] = useState([]);
+
+  useEffect(() =>{
+   
+    async function fetchPokemons(){
+      const response = await fetch (`/api/trainer/${trainerId}/pokemons`);
+      const pokemons = await response.json();
+      console.log(pokemons);
+      setPokemons(pokemons);
+    }
+    fetchPokemons();
+  },[])
+
+  return (
+    <>
+      {pokemons?.map((pokemon) => <div key={pokemon.species}><Pokemon pokemon={pokemon}></Pokemon></div>)}
+    </>
+  )
+}
+
+export default UserPage
