caa4860495677b779beb4ba61e5761cd1a6a880e
Papp Tommy <papptomi.23@gmail.com>
2024-05-08 21:26:57 +0200

    test: test deleteTrainer in TrainerService

--- my_pokemon_team/src/test/java/com/codecool/my_pokemon_team/service/TrainerServiceTest.java
+++ my_pokemon_team/src/test/java/com/codecool/my_pokemon_team/service/TrainerServiceTest.java
@@ -9,8 +9,11 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
+
 @ExtendWith(MockitoExtension.class)
 class TrainerServiceTest {
     @Mock
@@ -20,22 +23,38 @@ class TrainerServiceTest {
     private TrainerService trainerService;
 
     @Test
-    public void testAddTrainer_happyCase(){
+    public void testAddTrainer_happyCase() {
         //Arrange
         String trainerName = "TG";
         String password = "grookey1234";
         TrainerDTO expectedTrainerDTO = new TrainerDTO(trainerName, password);
 
-       /* Trainer trainerEntity = createTrainerEntity(trainerName, password);
-        when(trainerRepository.save(trainerEntity)).thenReturn(trainerEntity);*/
         //Act
         TrainerDTO actualTrainerDTO = trainerService.addTrainer(trainerName, password);
         //Assert
         assertEquals(expectedTrainerDTO, actualTrainerDTO);
     }
 
-    private Trainer createTrainerEntity(String name, String password) {
+    @Test
+    public void testDeleteTrainer_whenDelete_thenRemoveTrainer() {
+        //Arrange
+        String trainerName = "TG";
+        String password = "grookey1234";
+        Long id = 1L;
+        Trainer trainerEntity = createTrainerEntity(trainerName, password, id);
+        trainerRepository.save(trainerEntity);
+
+        //Act
+        trainerService.deleteTrainer(id);
+        Optional<Trainer> deletedTrainer = trainerRepository.findById(id);
+
+        //Assert
+        assertThat(deletedTrainer).isEmpty();
+    }
+
+    private Trainer createTrainerEntity(String name, String password, Long id) {
         Trainer trainerEntity = new Trainer();
+        trainerEntity.setId(id);
         trainerEntity.setTrainerName(name);
         trainerEntity.setPassword(password);
 
