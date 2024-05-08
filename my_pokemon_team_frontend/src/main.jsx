bb24b90927021db2a436f667217cf50febeba603
Sara Soos <142304198+soosdsara@users.noreply.github.com>
2024-05-08 13:15:15 +0200

    feat: create navigation bar

--- my_pokemon_team_frontend/src/main.jsx
+++ my_pokemon_team_frontend/src/main.jsx
@@ -1,28 +1,34 @@
-import React from 'react'
+import React from "react";
 import Login from "./Pages/Login.jsx";
-import ReactDOM from 'react-dom/client'
+import ReactDOM from "react-dom/client";
 import Search from "./Pages/Search.jsx";
-import UserPage from './Pages/UserPage.jsx'
-import Registration from './Pages/Registration.jsx';
+import UserPage from "./Pages/UserPage.jsx";
+import Registration from "./Pages/Registration.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./Pages/Layout/Layout.jsx";
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Login/>
-    },
-    {
-        path: "/registration",
-        element: <Registration/>
-    },
-    {
-        path: "/userpage/:id",
-        element: <UserPage/>
-    },
   {
-    path: "/userpage/:id/search",
-    element: <Search />,
-  }
+    path: "/",
+    element: <Login />,
+  },
+  {
+    path: "/registration",
+    element: <Registration />,
+  },
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { path: "/userpage/:id", 
+        element: <UserPage /> 
+      },
+      {
+        path: "/userpage/:id/search",
+        element: <Search />,
+      },
+    ],
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
