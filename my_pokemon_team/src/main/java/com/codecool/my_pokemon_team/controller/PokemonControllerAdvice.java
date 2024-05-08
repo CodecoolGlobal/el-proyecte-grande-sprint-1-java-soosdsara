d1b16373ac02483f78e3a987211ad32211dfe618
zarka1 <szucsbalazs367@gmail.com>
2024-05-08 12:00:43 +0200

    feat: handle No Valid User message on frontend, navigate to RegistrationPage

--- my_pokemon_team/src/main/java/com/codecool/my_pokemon_team/controller/PokemonControllerAdvice.java
+++ my_pokemon_team/src/main/java/com/codecool/my_pokemon_team/controller/PokemonControllerAdvice.java
@@ -1,2 +1,21 @@
-package com.codecool.my_pokemon_team.controller;public class PokemonControllerAdvice {
+package com.codecool.my_pokemon_team.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.NoSuchElementException;
+
+@ControllerAdvice
+public class PokemonControllerAdvice {
+    @ResponseBody
+    @ExceptionHandler(NoSuchElementException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String NoSuchElementExceptionHandler(NoSuchElementException ex)
+    {
+        return ex.getMessage();
+
+    }
 }
--- my_pokemon_team_frontend/src/Pages/UserPage.jsx
+++ my_pokemon_team_frontend/src/Pages/UserPage.jsx
@@ -1,15 +1,21 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react';
 import { useLocation } from "react-router-dom";
 import Pokemon from '../Components/Pokemon';
+import { useNavigate } from 'react-router';
 
 const UserPage = () => {
   const trainerId = useLocation().pathname.split("/userpage/")[1];
   const [pokemons, setPokemons] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() =>{
    
     async function fetchPokemons(){
       const response = await fetch (`/api/pokemons/${trainerId}`);
+      console.log(response.status)
+      if(response.status != 200){
+        navigate("/");
+      }
       const pokemons = await response.json();
       console.log(pokemons);
       setPokemons(pokemons);
@@ -19,7 +25,8 @@ const UserPage = () => {
 
   return (
     <>
-      {pokemons?.map((pokemon) => <div key={pokemon.species}><Pokemon pokemon={pokemon}></Pokemon></div>)}
+      {pokemons.length == 0 ? <label>You have currently no pokemons.</label>:
+       pokemons?.map((pokemon) => <div key={pokemon.species}><Pokemon pokemon={pokemon}></Pokemon></div>)}
     </>
   )
 }
