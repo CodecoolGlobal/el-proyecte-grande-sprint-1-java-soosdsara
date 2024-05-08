f4a7cbaed6b5b0850c08287351a3ae5bc12a36fe
Papp Tommy <papptomi.23@gmail.com>
2024-05-08 12:13:08 +0200

    update: update TrainerControllerAdvice

--- my_pokemon_team/src/main/java/com/codecool/my_pokemon_team/controller/TrainerControllerAdvice.java
+++ my_pokemon_team/src/main/java/com/codecool/my_pokemon_team/controller/TrainerControllerAdvice.java
@@ -1,7 +1,5 @@
 package com.codecool.my_pokemon_team.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
