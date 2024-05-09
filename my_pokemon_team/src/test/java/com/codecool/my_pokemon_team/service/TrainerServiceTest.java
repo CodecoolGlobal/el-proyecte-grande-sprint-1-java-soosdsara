b96d6930e54d0825acf6b274806f3f25dbef1317
Papp Tommy <papptomi.23@gmail.com>
2024-05-09 12:11:21 +0200

    test: test deleteTrainer and updatdTrainer

--- my_pokemon_team/src/test/java/com/codecool/my_pokemon_team/service/TrainerServiceTest.java
+++ my_pokemon_team/src/test/java/com/codecool/my_pokemon_team/service/TrainerServiceTest.java
@@ -11,18 +11,16 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.Optional;
-
-import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class TrainerServiceTest {
     @Captor
-    ArgumentCaptor<Trainer> captor;
+    ArgumentCaptor<Trainer> trainerCaptor;
+    @Captor
+    ArgumentCaptor<String> stringCaptor;
+
     @Mock
     private TrainerRepository trainerRepository;
 
@@ -40,66 +38,40 @@ class TrainerServiceTest {
         //Act
         trainerService.addTrainer(expectedTrainerDTO);
         //Assert
-        verify(trainerRepository).save(captor.capture());
-        Trainer trainer = captor.getValue();
+        verify(trainerRepository).save(trainerCaptor.capture());
+        Trainer trainer = trainerCaptor.getValue();
 
         assertEquals(expectedTrainer, trainer);
     }
 
     @Test
-    public void testAddTrainer_happyCase2() {
-        //Arrange
-        String trainerName = "TG";
-        String password = "grookey1234";
-        TrainerDTO expectedTrainerDTO = new TrainerDTO(trainerName, password);
-
-        when(trainerRepository.save(any(Trainer.class))).thenReturn(new Trainer());
-        //Act
-       trainerService.addTrainer(expectedTrainerDTO);
-        //Assert
-       // assertThat(actualTrainerDTO.name()).isSameAs(expectedTrainerDTO.name());
-    }
-
-
-   /* @Test
     public void testDeleteTrainer_whenDelete_thenRemoveTrainer() {
         //Arrange
         String trainerName = "TG";
-        String password = "grookey1234";
-
-        Trainer trainerEntity = createTrainerEntity(trainerName, password);
-        trainerRepository.save(trainerEntity);
-
         //Act
-        trainerService.deleteTrainer(id);
-        Optional<Trainer> deletedTrainer = trainerRepository.findById(id);
-
+        trainerService.deleteTrainer(trainerName);
         //Assert
-        assertThat(deletedTrainer).isEmpty();
-
-
+        verify(trainerRepository).deleteByTrainerName(stringCaptor.capture());
+        String deletedTrainerName = stringCaptor.getValue();
+        assertEquals(trainerName, deletedTrainerName);
     }
 
     @Test
     public void testUpdatePassword_whenUpdate_thenTrainerObject() {
         //Arrange
         String trainerName = "TG";
-        String password = "grookey1234";
-        Long id = 1L;
-        Trainer trainerEntity = createTrainerEntity(trainerName, password, id);
-        boolean trainerEntity2 = trainerRepository.findById(id).isPresent();
-        System.out.println(trainerEntity2);
-
+        String password = "pika";
         //Act
-        trainerService.updatePassword(id, "pikachu333");
-        boolean updatedTrainerEntity = trainerRepository.findById(id).isPresent();
-        System.out.println(updatedTrainerEntity);
+        trainerService.updatePassword(trainerName, password);
         //Assert
-        assertThat(updatedTrainerEntity).isNotNull();
-        //assertThat(updatedTrainerEntity.getPassword()).isEqualTo("pikachu333");
+        verify(trainerRepository).setTrainerEntityPasswordByTrainerName(stringCaptor.capture(), stringCaptor.capture());
+        String actual = stringCaptor.getAllValues().get(0);
+        String newPassword = stringCaptor.getAllValues().get(1);
 
+        assertEquals(actual, trainerName);
+        assertEquals(newPassword, password);
     }
-*/
+
     private Trainer createTrainerEntity(String name, String password) {
         Trainer trainerEntity = new Trainer();
         trainerEntity.setTrainerName(name);
@@ -107,5 +79,4 @@ class TrainerServiceTest {
 
         return trainerEntity;
     }
-
 }
