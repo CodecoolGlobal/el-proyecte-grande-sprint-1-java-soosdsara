b0055b46321bd993b296d516211c840ac8971158
zarka1 <szucsbalazs367@gmail.com>
2024-05-21 14:47:16 +0200

    feat: implemented userdetailsservice

--- my_pokemon_team/src/main/java/com/codecool/my_pokemon_team/controller/TrainerControllerAdvice.java
+++ my_pokemon_team/src/main/java/com/codecool/my_pokemon_team/controller/TrainerControllerAdvice.java
@@ -2,6 +2,7 @@ package com.codecool.my_pokemon_team.controller;
 
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -15,5 +16,14 @@ public class TrainerControllerAdvice {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public String DataIntegrityViolationExceptionHandler() {
         return "This trainer name already exists. Please choose another one.";
+    }
+
+    @ResponseBody
+    @ExceptionHandler(UsernameNotFoundException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String UsernameNotFoundExceptionHandler(
+            UsernameNotFoundException ex)
+    {
+        return ex.getMessage();
     }
 }
--- my_pokemon_team/src/main/java/com/codecool/my_pokemon_team/security/service/UserDetailsServiceImpl.java
+++ my_pokemon_team/src/main/java/com/codecool/my_pokemon_team/security/service/UserDetailsServiceImpl.java
@@ -1,12 +1,36 @@
 package com.codecool.my_pokemon_team.security.service;
 
+import com.codecool.my_pokemon_team.model.trainer.Trainer;
+import com.codecool.my_pokemon_team.repository.TrainerRepository;
+import com.codecool.my_pokemon_team.service.TrainerService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserDetailsServiceImpl implements UserDetailsService {
+    private final TrainerRepository trainerRepository;
+    private static final Logger logger = LoggerFactory.
+            getLogger(TrainerService.class);
+
+    public UserDetailsServiceImpl(TrainerRepository trainerRepository) {
+        this.trainerRepository = trainerRepository;
+    }
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        Trainer trainer = trainerRepository.findByTrainerName(username)
+                .orElseThrow(() -> new UsernameNotFoundException(username));
+
+        List<SimpleGrantedAuthority> roles = new ArrayList<>();
+
+        return new User(trainer.getTrainerName(), trainer.getPassword(), roles);
     }
+
 }
