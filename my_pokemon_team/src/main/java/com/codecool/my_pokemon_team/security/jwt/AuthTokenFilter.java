cfb2ab9abb0996b795726ffd23930dca2fd6fd28
zarka1 <szucsbalazs367@gmail.com>
2024-05-22 09:33:17 +0200

    style: format

--- my_pokemon_team/src/main/java/com/codecool/my_pokemon_team/security/jwt/AuthTokenFilter.java
+++ my_pokemon_team/src/main/java/com/codecool/my_pokemon_team/security/jwt/AuthTokenFilter.java
@@ -32,16 +32,13 @@ public class AuthTokenFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        //System.out.println(jwtUtils.validateJwtToken(parseJwt(request)));
         try {
             String jwt = parseJwt(request);
             if (jwt != null && jwtUtils.validateJwtToken(jwt)) {
                 String username = jwtUtils.getUserNameFromJwtToken(jwt);
-
                 UserDetails userDetails = userDetailsService.loadUserByUsername(username);
                 UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null,
                         userDetails.getAuthorities());
-                //System.out.println(authentication);
                 authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
                 System.out.println(authentication);
                 SecurityContextHolder.getContext().setAuthentication(authentication);
@@ -55,7 +52,6 @@ public class AuthTokenFilter extends OncePerRequestFilter {
 
     private String parseJwt(HttpServletRequest request) {
         String headerAuth = request.getHeader("Authorization");
-
         if (StringUtils.hasText(headerAuth) && headerAuth.startsWith("Bearer ")) {
             return headerAuth.substring(7);
         }
--- my_pokemon_team/src/main/java/com/codecool/my_pokemon_team/security/service/UserDetailsServiceImpl.java
+++ my_pokemon_team/src/main/java/com/codecool/my_pokemon_team/security/service/UserDetailsServiceImpl.java
@@ -5,20 +5,24 @@ import com.codecool.my_pokemon_team.repository.TrainerRepository;
 import com.codecool.my_pokemon_team.service.TrainerService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class UserDetailsServiceImpl implements UserDetailsService {
     private final TrainerRepository trainerRepository;
     private static final Logger logger = LoggerFactory.
             getLogger(TrainerService.class);
 
+    @Autowired
     public UserDetailsServiceImpl(TrainerRepository trainerRepository) {
         this.trainerRepository = trainerRepository;
     }
