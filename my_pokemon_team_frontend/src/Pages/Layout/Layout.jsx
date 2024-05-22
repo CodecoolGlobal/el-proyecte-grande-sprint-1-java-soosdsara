3bf5413e42e1446664158c07c974bdd51fd37bbb
Sara Soos <142304198+soosdsara@users.noreply.github.com>
2024-05-22 14:09:16 +0200

    feat: add logout button

--- my_pokemon_team_frontend/src/Pages/Layout/Layout.jsx
+++ my_pokemon_team_frontend/src/Pages/Layout/Layout.jsx
@@ -3,14 +3,22 @@ import { Outlet, NavLink, Link } from "react-router-dom";
 import "./Layout.css";
 
 const Layout = () => {
+
+  function handleLogout() {
+    localStorage.removeItem("trainerData");
+  }
+
   return (
     <div className="Layout">
       <nav>
       <Link to="/userpage">
-        <button className="Logo" >My Pokemon Team</button>
+        <button className="Logo">My Pokemon Team</button>
         </Link>
         <NavLink to={`/userpage/search`}>
           <button>Find a new pokemon</button>
+          <Link to="/">
+            <button onClick={handleLogout}>Log out</button>
+           </Link>
         </NavLink>
       </nav>
       <Outlet />
