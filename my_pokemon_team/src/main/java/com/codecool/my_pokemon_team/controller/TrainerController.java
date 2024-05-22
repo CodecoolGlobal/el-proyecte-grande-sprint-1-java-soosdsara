7c1110c6267cf02fa6b285010d341d73e2b1e078
zarka1 <szucsbalazs367@gmail.com>
2024-05-22 11:45:45 +0200

    feat: buildup login authentication system

--- my_pokemon_team/src/main/java/com/codecool/my_pokemon_team/controller/TrainerController.java
+++ my_pokemon_team/src/main/java/com/codecool/my_pokemon_team/controller/TrainerController.java
@@ -1,21 +1,36 @@
 package com.codecool.my_pokemon_team.controller;
 
 import com.codecool.my_pokemon_team.controller.dto.TrainerDTO;
+import com.codecool.my_pokemon_team.model.JwtResponse;
+import com.codecool.my_pokemon_team.security.jwt.JwtUtils;
 import com.codecool.my_pokemon_team.service.TrainerService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/trainer")
 public class TrainerController {
 
     private final TrainerService trainerService;
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtils jwtUtils;
 
 
-    public TrainerController(TrainerService trainerService, PasswordEncoder passwordEncoder) {
+    public TrainerController(TrainerService trainerService, PasswordEncoder passwordEncoder, AuthenticationManager authenticationManager, JwtUtils jwtUtils) {
         this.trainerService = trainerService;
+        this.authenticationManager = authenticationManager;
+
+        this.jwtUtils = jwtUtils;
     }
 
     @PostMapping
@@ -36,10 +51,14 @@ public class TrainerController {
     }
 
     @GetMapping("/{trainerName}")
-    public ResponseEntity<?> loginTrainer(@PathVariable String trainerName) {
-        if(trainerService.checkTrainerName(trainerName)) {
-            return ResponseEntity.ok("Trainer found");
-        }
-        return ResponseEntity.badRequest().body("No trainer with this username:" + trainerName);
+    public ResponseEntity<?> loginTrainer(@RequestBody TrainerDTO trainerDTO) {
+
+        Authentication authentication = authenticationManager
+                .authenticate(new UsernamePasswordAuthenticationToken(trainerDTO.name(), trainerDTO.password()));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = jwtUtils.generateJwtToken(authentication);
+        User userDetails = (User) authentication.getPrincipal();
+        return ResponseEntity
+                .ok(new JwtResponse(jwt, userDetails.getUsername()));
     }
 }
---
+++ my_pokemon_team/src/main/java/com/codecool/my_pokemon_team/model/JwtResponse.java
@@ -0,0 +1,4 @@
+package com.codecool.my_pokemon_team.model;
+
+public record JwtResponse (String jwt, String userName){
+}
