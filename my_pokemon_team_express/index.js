218ed87777c0e999adc5486b6c4c3e8b63755fca
Sara Soos <142304198+soosdsara@users.noreply.github.com>
2024-08-07 13:50:55 +0200

    feat: Optimaze project buildup and starting with Docker

--- my_pokemon_team_express/index.js
+++ my_pokemon_team_express/index.js
@@ -1,22 +1,19 @@
-const express = require('express')
-const { createProxyMiddleware } = require ('http-proxy-middleware')
+import express from 'express';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
 
 const app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-// Serve static files from the "dist" directory
 app.use('/api', createProxyMiddleware({
-    target: 'http://pokemon-backend-cont:8080/api', //dockercontainer name
+    target: `${process.env.BACKEND_URL}/api`,
     changeOrigin: true
 }));
 
-app.use(express.urlencoded({extended: true}))
-app.use(express.json())
-app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
 
-
-
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
